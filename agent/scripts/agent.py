f3ae74ff8bed4bd42acc611c6e5f9e735bd25245
arobaseSuulei <souleymanediallo2004@outlook.com>
2026-08-07 05:52:19 +0200

    feat V2: add agent script functionality

---
+++ agent/scripts/agent.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import argparse
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument("prompt", type=str)
+parser.add_argument("--verbose",action="store_true")
+args=parser.parse_args()
+prompt=sys.argv[1]
+
+print(f"> {prompt}")
+
+if args.verbose:
+    print("thinking...")
+
+
+client=OpenAI(api_key=OPENAI_API_KEY)
+
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input=prompt,
+)
+
+print(f"Token Output : {response.usage.output_tokens}")
+print(response.output_text)
+
+
+
